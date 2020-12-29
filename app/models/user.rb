e4f5c2b0d7cb81cc402d0385e5e3a537798ff8e1
hiromasa-horikoshi <hiromasa.horikoshi@gmail.com>
2020-12-30 08:24:18 +0900

    Userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/i }
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_reading, presence: true, format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
+  }
+  validates :first_name_reading, presence: true, format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
+  }
+  validates :birth_date, presence: true
+
+  has_many :items
+  has_many :orders
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+
+    nickname { Gimei.name }
+    email { Faker::Internet.free_email }
+    password = Faker::Internet.password(min_length: 6)
+    password { password }
+    password_confirmation { password }
+    last_name { person.last.kanji }
+    first_name { person.first.kanji }
+    last_name_reading { person.last.katakana }
+    first_name_reading { person.first.katakana }
+    birth_date { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
+  end
+end
