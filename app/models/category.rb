ab3f3a781d83d1fd87db8e3b4857014ab09f699a
hiromasa-horikoshi <hiromasa.horikoshi@gmail.com>
2020-12-31 15:13:13 +0900

    Statusモデルの作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -13,7 +13,7 @@ class Category < ActiveHash::Base
     { id: 11, name: 'その他' }
   ]
   
-   include ActiveHash::Associations
-   belongs_to :item
+    include ActiveHash::Associations
+    belongs_to :item
   end
 end
