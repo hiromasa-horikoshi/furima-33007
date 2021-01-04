c8dc77beb8d0373f4d8ec189eb5a38a26ad93010
hiromasa-horikoshi <hiromasa.horikoshi@gmail.com>
2021-01-04 11:27:02 +0900

    itemsコントローラー、コメントアウト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
 
   #商品一覧表示機能の実装の際にindexアクションに.order("created_at DESC")をつける
   def index
